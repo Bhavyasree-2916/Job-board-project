1235d52a8b195fcf5a79d472a2baa375086f27dd
Bhavyasree-2916 <jbhavyasree7709@gmail.com>
2026-03-11 14:47:23 +0530

    cors added

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-
+const cors = require("cors");
 const app = express();
 app.use(express.json());
+app.use(cors())
 
 /* ===============================
    DATABASE CONNECTION
